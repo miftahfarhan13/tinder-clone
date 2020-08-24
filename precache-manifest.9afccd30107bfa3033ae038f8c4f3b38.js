180c5a1fd66a4abc3a48418db3af79c4f243b162
miftahfarhan13 <miftahfarhan20@gmail.com>
2020-08-25 05:00:59 +0700

    Updates

---
+++ precache-manifest.9afccd30107bfa3033ae038f8c4f3b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d5fb641968d2fc028d6a26c4b43258",
+    "url": "/tinder-clone/index.html"
+  },
+  {
+    "revision": "083e08a2e6a5a9aa9270",
+    "url": "/tinder-clone/static/css/main.bc9b9f9b.chunk.css"
+  },
+  {
+    "revision": "fdbd31822edc00aebed3",
+    "url": "/tinder-clone/static/js/2.3db996dc.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tinder-clone/static/js/2.3db996dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083e08a2e6a5a9aa9270",
+    "url": "/tinder-clone/static/js/main.0fb91b42.chunk.js"
+  },
+  {
+    "revision": "de86c0115f6746d23e21",
+    "url": "/tinder-clone/static/js/runtime-main.acd27e62.js"
+  }
+]);
